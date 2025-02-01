ffb07d81382440524c1875044a734c3cc5cc9916
Laura Tenan <lauratenan@hotmail.com>
2025-01-31 21:17:05 -0300

    Funções para popular e atualizar a lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// Função para adicionar amigos
+function adicionarAmigo() {
+  let nome = document.querySelector("input").value;
+  if (!nome) {
+    alert("Por favor, insira um nome.");
+  } else {
+    amigos.push(nome);
+    atualizarListaAmigos();
+    limparCampo();
+  }
+}
+
+// Função para atualizar a lista de amigos
+function atualizarListaAmigos() {
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = " "; // Limpa a lista antes de atualizarmos
+
+    for (i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li"); // Cria um elemento <li>
+        item.textContent = amigos[i]; // Define o texto do <li>
+        lista.appendChild(item); // Adiciona o item a lista
+    }
+  }
+  
+  // Função para limpar o campo de entrada a cada inserção de um novo amigo
+  function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
+}
