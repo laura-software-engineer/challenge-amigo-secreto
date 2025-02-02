d79b2755f9ac2f48414e07076388a34d02e0350b
Laura Tenan <lauratenan@hotmail.com>
2025-02-02 19:15:06 -0300

    Função para realizar o sorteio do amigo secreto

--- app.js
+++ app.js
@@ -15,18 +15,29 @@ function adicionarAmigo() {
 
 // Função para atualizar a lista de amigos
 function atualizarListaAmigos() {
-    let lista = document.querySelector("#listaAmigos");
-    lista.innerHTML = " "; // Limpa a lista antes de atualizarmos
+  let lista = document.querySelector("#listaAmigos");
+  lista.innerHTML = " "; // Limpa a lista antes de atualizarmos
 
-    for (i = 0; i < amigos.length; i++) {
-        let item = document.createElement("li"); // Cria um elemento <li>
-        item.textContent = amigos[i]; // Define o texto do <li>
-        lista.appendChild(item); // Adiciona o item a lista
-    }
+  for (i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li"); // Cria um elemento <li>
+    item.textContent = amigos[i]; // Define o texto do <li>
+    lista.appendChild(item); // Adiciona o item a lista
   }
-  
-  // Função para limpar o campo de entrada a cada inserção de um novo amigo
-  function limparCampo() {
-    campo = document.querySelector('input');
-    campo.value = '';
 }
+
+// Função para limpar o campo de entrada a cada inserção de um novo amigo
+function limparCampo() {
+  campo = document.querySelector("input");
+  campo.value = "";
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Por favor, adicione amigos.")
+  } else {
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = amigoSorteado;
+  }
+}
+
